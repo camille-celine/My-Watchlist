a3e0fd7a6a60a5c4605562922bdef2075202aa3c
Camille Rey <camille.celine.rey@gmail.com>
2023-02-27 10:39:24 +0100

    add fetch

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export const Add = () => {
+  const [query, setQuery] = useState("");
+
+  const onChange = e => {
+    e.preventDefault();
+    setQuery(e.target.value);
+
+    fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${e.target.value}&key=${process.env.REACT_APP_GOOGLEBOOKS_KEY}&langRestrict=en`
+    )
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data)
+      });
+  };
   return (
-    <div>
-      <h1>Add Page</h1>
+    <div className="add-page">
+      <div className="container">
+        <div className="add-content">
+          <div className="input-wrapper">
+            <input type="text"
+              placeholder="Search for a book"
+              value ={query}
+              onChange={onChange}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
